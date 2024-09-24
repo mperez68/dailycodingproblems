17b61067c18996fc76104460aaaa9b3f48e55539
mperez68 <marc.perez.dev@gmail.com>
2024-09-24 00:32:46 -0700

    Given a string and a number of lines k, print the string in zigzag form.

--- src/ZigZagProblem/ZigZagProblem.java
+++ src/ZigZagProblem/ZigZagProblem.java
@@ -2,7 +2,6 @@ package ZigZagProblem;
 
 import main.AbstractProblem;
 
-import java.util.Arrays;
 import java.util.LinkedList;
 
 /**<p>Given a string and a number of lines k, print the string in zigzag form. In zigzag, characters are printed out diagonally from top left to bottom right until reaching the kth line, then back up to top right, and so on.</p>
