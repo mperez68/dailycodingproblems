aab773c58cc90a0f73b27ed34e1b1b8dedae823a
mperez68 <marc.perez.dev@gmail.com>
2024-09-24 10:58:05 -0700

    Write a function that rotates a list by k elements.

--- src/RotateListProblem/RotateListProblem.java
+++ src/RotateListProblem/RotateListProblem.java
@@ -2,10 +2,7 @@ package RotateListProblem;
 
 import main.AbstractProblem;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
 
 /**
  * <p>Write a function that rotates a list by k elements. For example, [1, 2, 3, 4, 5, 6] rotated by two becomes [3, 4, 5, 6, 1, 2]. Try solving this without creating a copy of the list. How many swap or move operations do you need?</p>
